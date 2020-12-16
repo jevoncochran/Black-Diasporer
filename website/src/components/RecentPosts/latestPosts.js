8651971057c636727fb8c594d90c113d1ec6ed9a
Jevon Cochran <jevon.cochran@gmail.com>
2020-12-15 19:58:07 -0800

    Recent Posts

---
+++ website/src/components/RecentPosts/latestPosts.js
@@ -0,0 +1,23 @@
+import dominicanRepublic from "../../assets/images/blog photos/dominican-republic.jpg";
+import friendsInIpanema from "../../assets/images/blog photos/friends-in-ipanema.JPG";
+import pelourinho from "../../assets/images/blog photos/pelourinho.JPG";
+
+const latestPosts = [
+  {
+    title: "How I Got Robbed in the Dominican Republic",
+    image: dominicanRepublic,
+    alt: "beautiful beach in the Dominican Republic",
+  },
+  {
+      title: "The Trip to Brazil That Nearly Ruined Our Friendship",
+      image: friendsInIpanema,
+      alt: "Jevon Cochran and friends on Ipanema beach in Rio de Janeiro, Brazil",
+    },
+    {
+      title: "New Beginnings in Bahia",
+      image: pelourinho,
+      alt: "Pelourinho in Salvador da Bahia, Brazil",
+    },
+];
+
+export default latestPosts;
